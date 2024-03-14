cf60b93bf47d8d19914436a5d7fb6676051b819f
Anoopmishra1 <anoopmishra72226@gmail.com>
2024-03-14 23:40:32 +0530

    react router

---
+++ 03project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // eslint-disable-next-line no-unused-vars
+  const [count] = useState(0)
+
+  return (
+    <h1 className="text-3xl font-bold underline bg-green-700">
+      Hello Anoop Mishra!
+    </h1>
+  )
+}
+
+export default App
